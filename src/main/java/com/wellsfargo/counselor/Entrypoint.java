f4e0180cd71f7f07f5639f62d00e0d06ca6916ec
KunalLabs <kunalchaudhari1200@gmail.com>
2025-06-20 08:48:00 +0530

    Update Entrypoint.java

--- src/main/java/com/wellsfargo/counselor/Entrypoint.java
+++ src/main/java/com/wellsfargo/counselor/Entrypoint.java
@@ -1,13 +1,1 @@
-package com.wellsfargo.counselor;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Entrypoint {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Entrypoint.class, args);
-    }
-
-}
